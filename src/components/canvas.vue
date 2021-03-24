bb644b7807bd4ae1bdad6a91a4cfaa251cdfc657
Yun <1931946508@qq.com>
2021-03-24 23:41:03 +0800

    ...

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -96,7 +96,7 @@ export default {
                     sprite.x = 20
                     sprite.y = 20
                     this.container.addChild(sprite)
-                    
+                    this.bindEvent(sprite, 'Image')
                     console.log(sprite.position, sprite.toGlobal(new Point(0, 0)))
 
                     // this.struct.push({
@@ -108,14 +108,15 @@ export default {
                 } else if (element.type === 'Circle') {
                     let circle = new Graphics()
                     circle.beginFill(0xc4c4c4)
-                    circle.drawCircle(200, 200, 32)
+                    circle.drawCircle(0, 0, 32)
                     circle.endFill()
-                    // circle.x = 0
-                    // circle.y = 300
+                    circle.x = 200
+                    circle.y = 200
                     // circle.position.set(400, 400)
                     circle.zIndex = 1
                     circle.interactive = true
                     circle.buttonMode = true
+
                     this.container.addChild(circle)
                     this.bindEvent(circle, 'Circle')
                 }
@@ -124,45 +125,127 @@ export default {
         },
         // 绑定事件
         bindEvent (graphics, type) {
-            if (type === 'Circle') {
+            if (type === 'Circle' || type === 'Image') {
                 let borderCir
                 let startPoint
                 let dragStatus = false
+                let line1, line2, line3, line4
+                let scalePoint1, scalePoint2, scalePoint3, scalePoint4
                 graphics.on('pointerover', () => {
-                    borderCir = new Graphics()
-                    borderCir.beginFill(0x18a0fb)
-                    borderCir.drawCircle(200, 200, 34)
-                    borderCir.endFill()
-                    borderCir.zIndex = 0
-                    this.container.addChild(borderCir)
+                    if (!borderCir) {
+                        if (type === 'Circle') {
+                            borderCir = new Graphics()
+                            borderCir.beginFill(0x18a0fb)
+                            borderCir.drawCircle(0, 0, 33.5)
+                            borderCir.endFill()
+                            borderCir.x = graphics.x
+                            borderCir.y = graphics.y
+                            borderCir.zIndex = 0
+                            this.container.addChild(borderCir)
+                        } else if (type === 'Image') {
+                            borderCir = new Graphics()
+                            borderCir.beginFill(0x18a0fb)
+                            borderCir.drawRect(0, 0, graphics.width + 4, graphics.height + 4)
+                            borderCir.endFill()
+                            borderCir.x = graphics.x - 2
+                            borderCir.y = graphics.y - 2
+                            borderCir.zIndex = 0
+                            this.container.addChild(borderCir)
+                        }
+                        
+                    }
                 })
                 graphics.on('pointerout', () => {
-                    borderCir.destroy()
+                    if (dragStatus && borderCir) return
+                    dragStatus = false
+                    borderCir && borderCir.destroy()
+                    borderCir = null
                 })
                 graphics.on('pointerdown', (event) => {
                     dragStatus = true
                     startPoint = {
                         x: event.data.global.x,
                         y: event.data.global.y
+                        // x: event.data.getLocalPosition(this.container).x,
+                        // y: event.data.getLocalPosition(this.container).y
                     }
+                    console.log(event.data.getLocalPosition(this.container))
+                    console.log(graphics.position)
                 })
                 graphics.on('pointerup', () => {
                     dragStatus = false
-                    borderCir.destroy()
+                    borderCir && borderCir.destroy()
+                    borderCir = null
+                })
+                graphics.on('pointertap', () => {
+                    if (type === 'Circle') {
+                        console.log('划线', graphics.width, graphics.x, graphics.y)
+                        let radius = graphics.width / 2 - 2
+                        line1 = this.drawDash({ x: graphics.x - radius, y: graphics.y - radius }, { x: graphics.x + radius, y: graphics.y - radius })
+                        line2 = this.drawDash({ x: graphics.x + radius, y: graphics.y - radius }, { x: graphics.x + radius, y: graphics.y + radius })
+                        line3 = this.drawDash({ x: graphics.x + radius, y: graphics.y + radius }, { x: graphics.x - radius, y: graphics.y + radius })
+                        line4 = this.drawDash({ x: graphics.x - radius, y: graphics.y + radius }, { x: graphics.x - radius, y: graphics.y - radius })
+                        
+                        // scalePoint1 = this.drawCir({ x: graphics.x - graphics.width / 2, y: graphics.y - graphics.width / 2 })
+                        // scalePoint2 = this.drawCir({ x: graphics.x + graphics.width / 2, y: graphics.y - graphics.width / 2 })
+                        // scalePoint3 = this.drawCir({ x: graphics.x + graphics.width / 2, y: graphics.y + graphics.width / 2 })
+                        // scalePoint4 = this.drawCir({ x: graphics.x - graphics.width / 2, y: graphics.y + graphics.width / 2 })
+                        let scaleRadius1 = graphics.width / 2 - 3
+                        let scaleRadius2 = graphics.width / 2 + 3
+                        scalePoint1 = this.drawRect(graphics.x - scaleRadius2, graphics.y - scaleRadius2, 6, 6)
+                        scalePoint2 = this.drawRect(graphics.x + scaleRadius1, graphics.y - scaleRadius2, 6, 6)
+                        scalePoint3 = this.drawRect(graphics.x + scaleRadius1, graphics.y + scaleRadius1, 6, 6)
+                        scalePoint4 = this.drawRect(graphics.x - scaleRadius2, graphics.y + scaleRadius1, 6, 6)
+                    }
                 })
                 graphics.on('pointermove', (event) => {
-                    if (dragStatus) {
+                    if (dragStatus && borderCir) {
+                        line1 && (line1.visible = false)
+                        line2 && (line2.visible = false)
+                        line3 && (line3.visible = false)
+                        line4 && (line4.visible = false)
+                        scalePoint1 && (scalePoint1.visible = false)
+                        scalePoint2 && (scalePoint2.visible = false)
+                        scalePoint3 && (scalePoint3.visible = false)
+                        scalePoint4 && (scalePoint4.visible = false)
                         const dx = event.data.global.x - startPoint.x
                         const dy = event.data.global.y - startPoint.y
-                        console.log(dx, dy)
+                        // const dx = event.data.getLocalPosition(this.container).x - startPoint.x
+                        // const dy = event.data.getLocalPosition(this.container).y - startPoint.y
+                        // console.log(dx, dy)
                         graphics.position.x += dx
                         graphics.position.y += dy
                         borderCir.position.x += dx
                         borderCir.position.y += dy
+                        console.log(graphics.position.x, graphics.position.y)
+                        // startPoint = { x: event.data.getLocalPosition(this.container).x, y: event.data.getLocalPosition(this.container).y }
                         startPoint = { x: event.data.global.x, y: event.data.global.y }
                     }
                 })
             }
+        },
+        // 划线
+        drawDash (startPoint, endPoint) {
+            console.log(startPoint, endPoint)
+            let line = new Graphics()
+            line.lineStyle(1.5, 0x18a0fb, 1, 2)
+            line.moveTo(startPoint.x, startPoint.y)
+            line.lineTo(endPoint.x, endPoint.y)
+            console.log(line.position)
+            this.container.addChild(line)
+            return line
+        },
+        // 画圆
+        drawRect (x, y, width, height) {
+            let rect = new Graphics()
+            rect.beginFill(0xffffff)
+            rect.lineStyle(1.5, 0x18a0fb, 1)
+            rect.drawRect(0, 0, width, height)
+            rect.endFill()
+            rect.x = x
+            rect.y = y
+            this.container.addChild(rect)
+            return rect
         }
     }
 }
